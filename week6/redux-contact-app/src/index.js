4da56be3ba766c7246638b536542db2fa8cce1bb
Seanerlars <Sean.larsen@bryanuniversity.edu>
2021-03-01 19:32:31 -0600

    redux

--- week6/redux-contact-app/src/index.js
+++ week6/redux-contact-app/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-
+const store = configureStore();
 
 ReactDOM.render(
-<Provider>
+<Provider store={store}>
     <App />
 </Provider>, document.getElementById('root'));
+
+
